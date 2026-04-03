ea7f4d7f0079bfce60ab6e01ffb8a075ba8ba86b
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-04-03 07:25:51 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 // Ross Core – Main JS File
+
 console.log("Ross Core loaded");
+
+// Initialise theme system
+import './theme.js';
+
+// Initialise dashboard interactions (only if dashboard exists)
+import './dashboard.js';
