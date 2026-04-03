66bb81960a3e1d2fe2a921fd7f6f39e3bfe54c68
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-04-03 08:36:53 +0100

    Update storage.js

--- assets/js/utils/storage.js
+++ assets/js/utils/storage.js
@@ -1,6 +1,7 @@
 // Storage Utilities
 
 export const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+
 export const load = (key, fallback = null) => {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : fallback;
